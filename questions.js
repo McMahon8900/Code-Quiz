45d052ae20b6dddcf2932b6d5c00b3db4e4637e2
Linsay McMahon <McMahon8900@gmail.com>
2019-11-17 22:10:32 -0600

    added in question structure

---
+++ questions.js
@@ -0,0 +1,35 @@
+let question = document.getElementById("questions");
+
+
+
+let questions = [{
+    //First Question
+    title: "Values can be passed into functions and used within then function. The name of the value is called a __________. The actual value itself is called an argument.",
+    choices: ["object", "parameter", "expression", "return"],
+    answer: "parameter"
+    },
+    {
+    //Second Question
+    title: "Which compay developed JavaScript?",
+    choices: ["Lime Wire", "Unknown", "Netscape", "Another Unknown"],
+    answer: "Netscape"
+    },
+    {
+    //Third Question
+    title: "_______ keyword refers to the object from where it was called.",
+    choices: ["document", "this", "element", "var"],
+    answer: "this"
+    },
+    {
+    //Fourth Question
+    title: "unknown question 4",
+    choices: ["1", "2", "3", "4"],
+    answer: "4"
+    },
+    {
+    //Fifth Question
+    title: "unknown question 5",
+    choices: ["1", "2", "3", "4"],
+    answer: "2"
+    }
+];
